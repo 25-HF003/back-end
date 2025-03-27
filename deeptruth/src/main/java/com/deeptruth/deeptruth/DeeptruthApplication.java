e4c62fa544322e56e59db68cd3982f34ab32ab35
Jihye0623 <wisdxx@gmail.com>
2025-03-27 11:52:54 +0900

    Setting

---
+++ deeptruth/src/main/java/com/deeptruth/deeptruth/DeeptruthApplication.java
@@ -0,0 +1,13 @@
+package com.deeptruth.deeptruth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeeptruthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeeptruthApplication.class, args);
+	}
+
+}
---
+++ deeptruth/src/test/java/com/deeptruth/deeptruth/DeeptruthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deeptruth.deeptruth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeeptruthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
