95c189cf78d8134d1c58309fa97c1f565439cbe3
YO1231 <whee0211@gmail.com>
2025-08-03 07:23:50 +0900

    Reapply "feat: JWT 인증 시스템 및 보호된 API"
    This reverts commit 02c4cab9ee75475b1154619ee8f324df9a3cfdc3.

---
+++ deeptruth/src/main/java/com/deeptruth/deeptruth/base/dto/user/UserProfile.java
@@ -0,0 +1,19 @@
+package com.deeptruth.deeptruth.base.dto.user;
+
+import com.deeptruth.deeptruth.base.Enum.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserProfile {
+    private Long userId;
+    private String loginId;
+    private String nickname;
+    private String email;
+    private Role role;
+}
