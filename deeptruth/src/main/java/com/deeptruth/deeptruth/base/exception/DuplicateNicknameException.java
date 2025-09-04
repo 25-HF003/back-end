526651222184bc6c787e995fe570db479915ef3d
YO1231 <whee0211@gmail.com>
2025-09-05 03:07:59 +0900

    Feat: 사용자 도메인 커스텀 예외 클래스 추가 (Green)
    - 이메일 중복 시 DuplicateEmailException
    - 닉네임 중복 시 DuplicateNicknameException
    - 비밀번호 검증 실패 시 InvalidPasswordException
    - 권한 부족 시 UnauthorizedOperationException
    
    기존 IllegalArgumentException 대신 의미 있는 예외 처리 제공

---
+++ deeptruth/src/main/java/com/deeptruth/deeptruth/base/exception/DuplicateNicknameException.java
@@ -0,0 +1,7 @@
+package com.deeptruth.deeptruth.base.exception;
+
+public class DuplicateNicknameException extends RuntimeException {
+    public DuplicateNicknameException(String message) {
+        super(message);
+    }
+}
---
+++ deeptruth/src/main/java/com/deeptruth/deeptruth/base/exception/UnauthorizedOperationException.java
@@ -0,0 +1,7 @@
+package com.deeptruth.deeptruth.base.exception;
+
+public class UnauthorizedOperationException extends RuntimeException {
+    public UnauthorizedOperationException(String message) {
+        super(message);
+    }
+}
